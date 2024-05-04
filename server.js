4a7727251c755ba5e8c7150c5006991e537fa5d6
Vishakhaverma2718 <vishakhaverma8085@gmail.com>
2024-05-05 03:08:57 +0530

    All files added

--- server.js
+++ server.js
@@ -1,32 +1,4 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import {dbConnection} from "./database/dbConnection.js";
-import { errorMiddleware } from "./middlewares/error.js";
-import userRouter from "./routes/user.js";
-
-const app = express();
-dotenv.config({path : "./config/config.env"});
-
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods : ["POST","GET","PUT","DELETE"],
-    credentials : true,
-})
-);
-
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-
-app.use('/',userRouter);
-
-app.get("/", (req, res, next)=>{return res.status(200).json({
-    success: true,
-    message: "HELLO WORLD AGAIN"
-})})
-dbConnection();
-app.use(errorMiddleware)
-
+import app from "./app.js";
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server Running On Port ${process.env.PORT}`);
